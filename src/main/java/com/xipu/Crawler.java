d2e758ce4a88b77ed8d245e04678063f694aaec1
Xi Pu <xp19@duke.edu>
2020-01-16 11:46:12 -0500

    implement basic logic flow

--- src/main/java/com/xipu/Crawler.java
+++ src/main/java/com/xipu/Crawler.java
@@ -2,6 +2,7 @@ package com.xipu;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -9,31 +10,51 @@ import java.util.*;
 
 public class Crawler {
     public static void main(String[] args) throws IOException {
-
         Queue<String> linkPool = new LinkedList<>();
         Set<String> processedLinks = new HashSet<>();
         linkPool.offer("https://sina.cn/");
 
         String linkToBeProcessed;
 
-        while ( (linkToBeProcessed = linkPool.remove()) != null ) {
+        while ((linkToBeProcessed = linkPool.remove()) != null) {
             if (processedLinks.contains(linkToBeProcessed)) {
                 continue;
             }
-
-            linkPool.addAll(get(linkToBeProcessed));
-            processedLinks.add(linkToBeProcessed);
+            System.out.println(linkToBeProcessed);
+            if (isNewsLink(linkToBeProcessed)) {
+                Document doc = Jsoup.connect(linkToBeProcessed).get();
+                processNewsLink(doc);
+                linkPool.addAll(getAllLinksOnThisPage(doc));
+                processedLinks.add(linkToBeProcessed);
+            }
         }
-
     }
 
-    private static List<String> get(String link) throws IOException {
-        System.out.println(link);
-        Document doc = Jsoup.connect(link).get();
+    private static void processNewsLink(Document doc) {
+        Elements articleTags = doc.select("article");
+        if (articleTags.size() > 0) {
+            System.out.println(articleTags.get(0).select("h1").text());
+            Elements content = articleTags.get(0).select(".art_p");
+            for (Element paragraph : content) {
+                System.out.println(paragraph.text());
+            }
+        }
+    }
+
+    private static List<String> getAllLinksOnThisPage(Document doc) {
         Elements linkTags = doc.select("a");
 
         List<String> result = new ArrayList<>();
         linkTags.forEach(element -> result.add(element.attr("href")));
         return result;
+    }
+
+    private static boolean isNewsLink(String link) {
+        return isValidLink(link) && link.contains("sina.cn") && !link.contains("photo.sina.cn");
+    }
+
+    private static boolean isValidLink(String link) {
+        link = link.toLowerCase();
+        return !link.contains("javascript") && link.length() != 0;
     }
 }
