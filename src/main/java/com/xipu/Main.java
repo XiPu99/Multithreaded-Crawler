bd7f98a01965b27b73b209bff71283f3855f9a3d
Xi Pu <xp19@duke.edu>
2020-01-19 17:30:34 -0500

    fix checkstyle errors

--- src/main/java/com/xipu/Main.java
+++ src/main/java/com/xipu/Main.java
@@ -3,7 +3,7 @@ package com.xipu;
 public class Main {
     public static void main(String[] args) {
         CrawlerDao dao = new MybatisDao();
-        for(int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++) {
             new Crawler(dao).start();
         }
     }
